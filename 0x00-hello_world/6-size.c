599c1f7ee3fa31fef935ab57d02b7dc8858e5ec6
Coulibaly Sarah Tiéhoua <sarah.coulibaly@uvci.edu.ci>
2022-10-17 23:09:03 -0700

    enfin

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	long long int trefl;
+	long long int trl;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(trefl));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(trl));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(float));
 	return (0);
 }
