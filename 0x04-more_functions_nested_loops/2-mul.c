d4f9bb43b0e31852de50e79c4ea2c2a373492e22
Coulibaly Sarah Tiéhoua <sarah.coulibaly@uvci.edu.ci>
2022-10-20 15:25:48 -0700

    exercice of multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - multiply two numbers
+ *
+ *@a: First parameter to be multiply.
+ *@b: second parameter to be multiply.
+ *
+ *Return: params
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
