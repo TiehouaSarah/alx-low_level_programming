47cb4591d288d2faf48fa2881eaa6039a2c41ff7
Coulibaly Sarah Tiéhoua <sarah.coulibaly@uvci.edu.ci>
2022-10-20 15:51:43 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - check the if param is upperCase
+ *
+ *Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
