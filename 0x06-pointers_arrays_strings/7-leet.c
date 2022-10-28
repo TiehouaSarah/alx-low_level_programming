c7833634719ae838a1bef9fcc77b650c4b34d112
Coulibaly Sarah Tiéhoua <sarah.coulibaly@uvci.edu.ci>
2022-10-27 22:48:46 -0700

    Write a function that encodes a string into 1337.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+
+/**
+ * leet - function encode a word to 1337
+ *
+ * @s : pointer pass as param
+ *
+ *Return: pointer converted
+ */
+
+char *leet(char *s)
+{
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[a])
+	{
+		b = 0;
+		while (b < l)
+		{
+			if (s[a] == r[b] || s[a] - 32 == r[b])
+				s[a] = n[b];
+			b++;
+		}
+		a++;
+	}
+	return (s);
+}
