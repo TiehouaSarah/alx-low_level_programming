bc8947298b19d7e0d444536e977a02040303a34a
Coulibaly Sarah Tiéhoua <sarah.coulibaly@uvci.edu.ci>
2022-10-30 08:28:11 -0700

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "_putchar.c"
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char n;
+
+	for(n = 'a'; i <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
