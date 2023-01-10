4aa4ce7481e84acf42d5ad70501e2d59d898438b
Tiehoua Sarah <sarahjc225@gmail.com>
2023-01-10 10:02:05 -0800

    correction of exercice 1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,8 +6,9 @@
  * @size: first parameter
  * @c: second parameter
  *
- * Return NULL if size = 0 / a pointer to the array, or NULL if it fails
+ * Description: nothing to say
  *
+ * Return: Return NULL if size = 0 / pointer to array/NULL if it fails
  */
 
 char *create_array(unsigned int size, char c)
