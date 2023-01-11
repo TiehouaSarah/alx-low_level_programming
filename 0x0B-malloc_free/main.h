a435a5be217e4ab29f851a2d95520ff59ad6a8de
Tiehoua Sarah <sarahjc225@gmail.com>
2023-01-11 09:21:49 -0800

    exercice 4

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,3 +3,4 @@ char *create_array(unsigned int size, char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_strdup(char *str);
 int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
