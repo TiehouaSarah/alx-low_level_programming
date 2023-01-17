3ee4cd5a69424cca9ab99465bcc5ab445c28be72
Tiehoua Sarah <sarahjc225@gmail.com>
2023-01-16 20:19:52 -0800

    pushing first exercice

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - creating dog structure
+ *
+ * @name : first param
+ * @age : second param
+ * @owner : third param
+ * Description: nothing to say
+ *
+ * Return:nothing
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
