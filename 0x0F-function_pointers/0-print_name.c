703d6cacdd25340c287c0d98218a6728137a3949
Tiehoua Sarah <sarahjc225@gmail.com>
2023-01-18 08:45:29 -0800

    calling someOne

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_name - is a pointer to function that prints name
+ *
+ * @name : name to print
+ * @f : function to pointe
+ *
+ * Return : return void
+ *
+ * */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * function_pointers - pointers to function
+ *
+ * Return:nothing
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
