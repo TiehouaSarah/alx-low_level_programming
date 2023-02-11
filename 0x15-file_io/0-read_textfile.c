f638bfd6f3590f76135a537cfc67fa007b111778
Tiehoua Sarah <sarahjc225@gmail.com>
2023-02-10 17:30:39 -0800

    recap

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - function that creates a file
+ * @filename: first parameter
+ * @letters: second parameter
+ *
+ * Return: int value
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char *ch = malloc(sizeof(char) * (letters));
+	ssize_t size = 0;
+
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
+	if (fp == NULL)
+		return (0);
+
+	while (!feof(fp))
+	{
+		if(fgets(ch, letters, fp) != NULL)
+			printf("%s", fgets(ch, letters, fp));
+		size++;
+	}
+
+	fclose(fp);
+	free(ch);
+
+	return (size);
+}
