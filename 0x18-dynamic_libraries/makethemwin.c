906f18fea25064c1c9e4f4d7f44c2b752962553e
Tiehoua Sarah <sarahjc225@gmail.com>
2023-03-22 11:18:53 -0700

    ddd

---
+++ 0x18-dynamic_libraries/makethemwin.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * makethemwin - additionne of two number
+ *
+ * Return: the resut
+ */
+
+int makethemwin(void)
+{
+	printf("%d %d %d %d %d", 9, 8, 10, 24, 75, 9);
+	return (0);
+}
